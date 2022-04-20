417599ec424c0b97ffc13a28198612fc5e2fac3e
LK060963 <LK060963@cerner.net>
2022-04-19 20:11:14 -0500

    following along

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Greeting() {
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*
+const Greeting = () => {
+  return React.createElement('h1', {}, 'hello world')
+}*/
+
+ReactDom.render(<Greeting />, document.getElementById("root"));
